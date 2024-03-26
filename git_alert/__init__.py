ffc1f5fdc96dbd2d16c09adb7d3319bb566314f5
Simon Antonius Lauer <simon.lauer@posteo.de>
2024-03-26 21:22:01 +0100

    feat: reduced if/else in main
    There is no need for it as the argparser sets the path to cwd by default
    if no path is provided.

--- git_alert/__init__.py
+++ git_alert/__init__.py
@@ -1,5 +1,4 @@
 from argparse import Namespace
-from pathlib import Path
 
 from argument_parser import argument_parser
 from repositories import Repositories
@@ -9,12 +8,7 @@ from traverse import traverse_directory
 def main(args: Namespace) -> None:
     repos = Repositories()
 
-    if args.path.is_dir():
-        traverse_directory(args.path, repos, args.verbose, args.only_dirty)
-    else:
-        wd = Path.cwd()
-        print(f"Traversing directory {wd}")
-        traverse_directory(wd, repos, args.verbose, args.only_dirty)
+    traverse_directory(args.path, repos, args.verbose, args.only_dirty)
 
     repos.display(only_dirty=args.only_dirty)
 
