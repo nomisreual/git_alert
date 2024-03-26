c7caaba5d00d6852124a669be60ba2d9d930c52b
Simon Antonius Lauer <simon.lauer@posteo.de>
2024-03-26 21:49:12 +0100

    docs: more details to README, better --help

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Git Alert
+
+This is a Python application that checks in the given directory and all its subdirectories
+for any dirty repositories.
+
+This application aims to ease the frequent use of git as it makes it easy to check for any untracked changes in any of your repositories.
+
+## Usage:
+
+```
+usage: __init__.py [-h] [--path PATH] [--only_dirty]
+
+options:
+  -h, --help    show this help message and exit
+  --path PATH
+  --only_dirty
+```
--- git_alert/argument_parser.py
+++ git_alert/argument_parser.py
@@ -4,6 +4,15 @@ from pathlib import Path
 
 def argument_parser() -> Namespace:
     parser = ArgumentParser()
-    parser.add_argument("--path", type=Path, default=Path.cwd())
-    parser.add_argument("--only_dirty", action="store_true")
+    parser.add_argument(
+        "--path",
+        type=Path,
+        default=Path.cwd(),
+        help="Top level directory to start the search in",
+    )
+    parser.add_argument(
+        "--only_dirty",
+        action="store_true",
+        help="In the final report only show dirty repositories",
+    )
     return parser.parse_args()
