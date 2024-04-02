480d24fc756213e6a30058494acd0c63c2ed1d65
Simon Antonius Lauer <simon.lauer@posteo.de>
2024-04-02 15:11:56 +0200

    feat: split traverse
    prior: traversing and checking in one function
    now: split in two

--- git_alert/traverse.py
+++ git_alert/traverse.py
@@ -9,16 +9,20 @@ def traverse_directory(pth: Path, repos: Repositories) -> None:
     files = pth.glob("*")
     for file in files:
         if file.is_dir() and file.name == ".git":
-            repo = {}
-            output = subprocess.run(
-                ["git", "status"], cwd=file.parent, stdout=subprocess.PIPE
-            )
-            if "working tree clean" in output.stdout.decode():
-                repo[file.parent] = "clean"
-                repos.add_repo(repo)
-            else:
-                repo[file.parent] = "dirty"
-                repos.add_repo(repo)
+            check_status(file, repos)
 
         elif file.is_dir():
             traverse_directory(file, repos)
+
+
+def check_status(directory: Path, repos: Repositories) -> None:
+    repo = {}
+    output = subprocess.run(
+        ["git", "status"], cwd=directory.parent, stdout=subprocess.PIPE
+    )
+    if "working tree clean" in output.stdout.decode():
+        repo[directory.parent] = "clean"
+        repos.add_repo(repo)
+    else:
+        repo[directory.parent] = "dirty"
+        repos.add_repo(repo)
