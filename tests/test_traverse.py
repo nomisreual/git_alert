1d09e2b613d9b2d8288880afede33656d82a36e6
Simon Antonius Lauer <simon.lauer@posteo.de>
2024-04-19 15:36:18 +0200

    test: test case for subdir traversing

--- tests/test_traverse.py
+++ tests/test_traverse.py
@@ -64,6 +64,49 @@ class TestGitAlertTraverse(unittest.TestCase):
         # Test if check method is not called:
         alert.check.assert_not_called()
 
+    @patch("git_alert.traverse.Path")
+    def test_traverse_only_dir(self, mock_path):
+        # Mock the return value of the glob method:
+        glob_result = []
+
+        # Mock one path that is a git repository:
+        dir = Mock()
+        dir.is_dir.return_value = True
+        dir.name = "some_dir"
+
+        # Add the mocked git directory to the glob result:
+        glob_result.append(dir)
+        # The return value of glob needs to be iterable:
+        mock_path.glob.return_value = iter(glob_result)
+
+        # Mock subdir
+        glob_result_sub_dir = []
+
+        # Mock one file:
+        sub_dir = Mock()
+        sub_dir.is_dir.return_value = False
+        sub_dir.name = "some_file"
+
+        glob_result_sub_dir.append(sub_dir)
+        dir.glob.return_value = iter(glob_result_sub_dir)
+
+        # Create GitAlert instance
+        # Check is not really executed, hence a simple Mock as
+        # repos argument suffices:
+        alert = GitAlert(mock_path, Mock())
+        # Mock the check method so its invocation can be tested:
+        alert.check = MagicMock(name="check")
+
+        alert.traverse(mock_path)
+
+        # Test if glob is called correctly:
+        mock_path.glob.assert_called_once_with("*")
+
+        # Test if subdir is looked through:
+        dir.glob.assert_called_once_with("*")
+        # Test if check method is NOT called:
+        alert.check.assert_not_called()
+
 
 class TestGitAlertTraversePermissionDenied(unittest.TestCase):
     @patch("git_alert.traverse.print")
