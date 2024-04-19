2e813a045986335efa9db6bf1820cbcd0e8b592e
Simon Antonius Lauer <simon.lauer@posteo.de>
2024-04-19 19:46:59 +0200

    feat: nix package build
    Can be built using nix build.

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "git_alert";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    poetry2nix = {
+      url = "github:nix-community/poetry2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    poetry2nix,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+      inherit (poetry2nix.lib.mkPoetry2Nix {inherit pkgs;}) mkPoetryApplication;
+    in {
+      packages = {
+        git_alert = mkPoetryApplication {
+          projectDir = self;
+        };
+        default = self.packages.${system}.git_alert;
+      };
+    });
+}
