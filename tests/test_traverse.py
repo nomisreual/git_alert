e9d97cc0691d82a000b53caa7966c0039311b053
Simon Antonius Lauer <simon.lauer@posteo.de>
2024-04-23 14:15:19 +0200

    test: updated test cases
    These changes now reflect the recent change in which .traverse() does
    not invoke .check() directly.
    
    Also, the tests for .check() are now updated.

--- tests/test_traverse.py
+++ tests/test_traverse.py
@@ -8,32 +8,32 @@ from git_alert.traverse import GitAlert
 class TestGitAlertTraverse(unittest.TestCase):
     @patch("git_alert.traverse.Path")
     def test_traverse_git_present(self, mock_path):
-        # Mock the return value of the glob method:
-        glob_result = []
-
         # Mock one path that is a git repository:
         dir = Mock()
         dir.is_dir.return_value = True
         dir.name = ".git"
+        dir.parent = "/parent"
 
         # Add the mocked git directory to the glob result:
+        glob_result = []
         glob_result.append(dir)
         # The return value of glob needs to be iterable:
         mock_path.glob.return_value = iter(glob_result)
 
+        # Mock repos of GitAlert instance:
+        repos = Mock()
+        repos.add_repo = MagicMock(name="add_repo")
+
         # Create GitAlert instance
-        # Check is not really executed, hence a simple Mock as
-        # repos argument suffices:
-        alert = GitAlert(mock_path, Mock())
-        # Mock the check method so its invocation can be tested:
-        alert.check = MagicMock(name="check")
+        alert = GitAlert(mock_path, repos)
 
         alert.traverse(mock_path)
 
         # Test if glob is called correctly:
         mock_path.glob.assert_called_once_with("*")
-        # Test if check method is called correctly:
-        alert.check.assert_called_once_with(dir)
+
+        # Check whether .add_repo was called with the correct argument:
+        repos.add_repo.assert_called_once_with({"path": "/parent", "status": None})
 
     @patch("git_alert.traverse.Path")
     def test_traverse_git_not_present(self, mock_path):
@@ -50,19 +50,20 @@ class TestGitAlertTraverse(unittest.TestCase):
         # The return value of glob needs to be iterable:
         mock_path.glob.return_value = iter(glob_result)
 
+        # Mock repos of GitAlert instance:
+        repos = Mock()
+        repos.add_repo = MagicMock(name="add_repo")
+
         # Create GitAlert instance
-        # Check is not really executed, hence a simple Mock as
-        # repos argument suffices:
-        alert = GitAlert(mock_path, Mock())
-        # Mock the check method so its invocation can be tested:
-        alert.check = MagicMock(name="check")
+        alert = GitAlert(mock_path, repos)
 
         alert.traverse(mock_path)
 
         # Test if glob is called correctly:
         mock_path.glob.assert_called_once_with("*")
-        # Test if check method is not called:
-        alert.check.assert_not_called()
+
+        # Make sure no 'repo' was added:
+        repos.add_repo.assert_not_called()
 
     @patch("git_alert.traverse.Path")
     def test_traverse_only_dir(self, mock_path):
@@ -90,22 +91,22 @@ class TestGitAlertTraverse(unittest.TestCase):
         glob_result_sub_dir.append(sub_dir)
         dir.glob.return_value = iter(glob_result_sub_dir)
 
+        # Mock repos of GitAlert instance:
+        repos = Mock()
+        repos.add_repo = MagicMock(name="add_repo")
+
         # Create GitAlert instance
-        # Check is not really executed, hence a simple Mock as
-        # repos argument suffices:
-        alert = GitAlert(mock_path, Mock())
-        # Mock the check method so its invocation can be tested:
-        alert.check = MagicMock(name="check")
+        alert = GitAlert(mock_path, repos)
 
         alert.traverse(mock_path)
 
         # Test if glob is called correctly:
         mock_path.glob.assert_called_once_with("*")
-
         # Test if subdir is looked through:
         dir.glob.assert_called_once_with("*")
-        # Test if check method is NOT called:
-        alert.check.assert_not_called()
+
+        # Make sure no 'repo' was added:
+        repos.add_repo.assert_not_called()
 
 
 class TestGitAlertTraversePermissionDenied(unittest.TestCase):
@@ -138,18 +139,18 @@ class TestGitAlertCheck(unittest.TestCase):
         output.stdout.decode.return_value = "working tree clean"
         mock_subprocess.run.return_value = output
 
-        pth = Mock()
-        pth.parent = "/parent"
-
-        # Create GitAlert instance
+        # List of one repository:
         repos = Mock()
+        repos.repos = [{"path": "/directory", "status": None}]
+
+        # Create GitAlert instance:
         alert = GitAlert(Mock(), repos)
 
-        # Call the check method on the mocked path:
-        alert.check(pth)
-        # Assert whether the mocked repo's add_repo method was calles
-        # appropriately:
-        repos.add_repo.assert_called_once_with({"/parent": "clean"})
+        # Call the .check method:
+        alert.check()
+
+        # Verify that the list of repositories was updated correctly:
+        self.assertEqual(repos.repos, [{"path": "/directory", "status": "clean"}])
 
     @patch("git_alert.traverse.subprocess")
     def test_git_alert_check_dirty(self, mock_subprocess):
@@ -158,18 +159,18 @@ class TestGitAlertCheck(unittest.TestCase):
         output.stdout.decode.return_value = "Changes not staged for commit"
         mock_subprocess.run.return_value = output
 
-        pth = Mock()
-        pth.parent = "/parent"
-
-        # Create GitAlert instance
+        # List of one repository:
         repos = Mock()
+        repos.repos = [{"path": "/directory", "status": None}]
+
+        # Create GitAlert instance:
         alert = GitAlert(Mock(), repos)
 
-        # Call the check method on the mocked path:
-        alert.check(pth)
-        # Assert whether the mocked repo's add_repo method was calles
-        # appropriately:
-        repos.add_repo.assert_called_once_with({"/parent": "dirty"})
+        # Call the .check method:
+        alert.check()
+
+        # Verify that the list of repositories was updated correctly:
+        self.assertEqual(repos.repos, [{"path": "/directory", "status": "dirty"}])
 
 
 class TestGitAlertRepos(unittest.TestCase):
