05f7d9d741bdb819917fa6b4e00e1abceabebf48
Simon Antonius Lauer <simon.lauer@posteo.de>
2024-04-28 13:46:23 +0200

    feat: rich output now in one class
    'Rich' output handling was previously handled with two classes wich adds
    code duplication without any tangible upside. Hence, it was combined
    into one class.

--- git_alert/display.py
+++ git_alert/display.py
@@ -4,50 +4,45 @@ from rich.table import Table
 from git_alert.repositories import Repositories
 
 
-class Display:
-    def __init__(self, repos: Repositories, only_dirty: bool) -> None:
-        self.console = Console()
-        self.repos = repos.repos
-        self.only_dirty = only_dirty
-
-    def create_table(self) -> Table:
-        self.table = Table(title="Full Report")
-        self.table.add_column("Project", style="cyan", justify="left")
-        self.table.add_column("Status", style="magenta", justify="center")
-        self.table.add_column("Full Path", style="blue", justify="left")
-
-    def populate_table(self) -> None:
-        """
-        Add project, status and full path of each found repository.
-        """
-        for pth, repo in self.repos.items():
-            status = repo.get("status")
-            if self.only_dirty and status == "clean":
-                continue
-            self.table.add_row(str(pth.name), status, str(pth))
-
-    def display(self) -> None:
-        self.console.print(self.table)
-
-
-class Summary:
+class Report:
     def __init__(self, repos: Repositories, only_dirty: bool) -> None:
         self.console = Console()
         self.repos = repos
         self.only_dirty = only_dirty
 
-    def create_table(self) -> Table:
-        self.table = Table(title="Summary Report")
-        self.table.add_column("Number of Repositories", style="cyan", justify="center")
-        self.table.add_column(
+    def create_long_report_table(self) -> None:
+        self.long_report = Table(title="Full Report")
+        self.long_report.add_column("Project", style="cyan", justify="left")
+        self.long_report.add_column("Status", style="magenta", justify="center")
+        self.long_report.add_column("Full Path", style="blue", justify="left")
+
+    def create_summary_table(self) -> None:
+        self.summary_report = Table(title="Summary Report")
+        self.summary_report.add_column(
+            "Number of Repositories", style="cyan", justify="center"
+        )
+        self.summary_report.add_column(
             "Number of Dirty Repositories", style="magenta", justify="center"
         )
 
-    def populate_table(self) -> None:
-        self.table.add_row(
+    def populate_long_report_table(self) -> None:
+        """
+        Add project, status and full path of each found repository.
+        """
+        for pth, repo in self.repos.repos.items():
+            status = repo.get("status")
+            if self.only_dirty and status == "clean":
+                continue
+            self.long_report.add_row(str(pth.name), status, str(pth))
+
+    def populate_short_table(self) -> None:
+        self.summary_report.add_row(
             str(self.repos.number_of_repositories),
             str(self.repos.number_of_dirty_repositories),
         )
 
-    def display(self) -> None:
-        self.console.print(self.table)
+    def display_long_report(self) -> None:
+        self.console.print(self.long_report)
+
+    def display_summary_report(self) -> None:
+        self.console.print(self.summary_report)
