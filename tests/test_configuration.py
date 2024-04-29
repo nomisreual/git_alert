50eb466000a178420b678a4f5de40064e9e90a52
Simon Antonius Lauer <simon.lauer@posteo.de>
2024-04-29 23:21:39 +0200

    fix: TestSystemLinux testcase
    User of runner on GitHub is not called "simon", hence mocking the USER
    environmental variable.

--- tests/test_configuration.py
+++ tests/test_configuration.py
@@ -1,3 +1,4 @@
+import os
 import tomllib
 import unittest
 from pathlib import Path
@@ -7,15 +8,20 @@ from git_alert.configuration import ReadConfig, System
 
 
 class TestSystemLinux(unittest.TestCase):
-    @patch("sys.platform", "linux")
-    def test_system(self):
+    @patch("os.environ.get")
+    def test_system(self, mock_user):
+        mock_user.return_value = "test_user"
         system = System()
         self.assertEqual(
-            system.config_root, Path("/home") / "simon" / ".config/git_alert"
+            system.config_root,
+            Path("/home") / str(os.environ.get("USER")) / ".config/git_alert",
         )
         self.assertEqual(
             system.config_file,
-            Path("/home") / "simon" / ".config/git_alert" / "config.toml",
+            Path("/home")
+            / str(os.environ.get("USER"))
+            / ".config/git_alert"
+            / "config.toml",
         )
 
 
@@ -69,17 +75,20 @@ class TestReadConfig(unittest.TestCase):
         mock_load.assert_called_once()
         self.assertTrue(config.only_dirty)
 
-    def test_read_config_decode_error(self):
-        with patch("git_alert.configuration.tomllib.load") as mock_load:
-            # Add side effect to the mock_file object to raise a FileNotFoundError
-            mock_load.side_effect = tomllib.TOMLDecodeError()
+    @patch("git_alert.configuration.tomllib.load")
+    # @patch("os.environ.get")
+    def test_read_config_decode_error(self, mock_load):
+        # mock_user.return_value = "test_user"
+        # with patch("git_alert.configuration.tomllib.load") as mock_load:
+        # Add side effect to the mock_file object to raise a FileNotFoundError
+        mock_load.side_effect = tomllib.TOMLDecodeError()
 
-            # Call the ReadConfig init method:
-            config = ReadConfig(System())
+        # Call the ReadConfig init method:
+        config = ReadConfig(System())
 
-            # Make assertions:
-            self.assertEqual(config._config, {})
-            mock_load.assert_called_once()
+        # Make assertions:
+        self.assertEqual(config._config, {})
+        mock_load.assert_called_once()
 
     def test_read_config_custom_path(self):
         config = ReadConfig(System(), config="some/path")
