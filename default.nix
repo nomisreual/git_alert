b860591bc4e8356572ccd62015f6099b890a418a
Simon Antonius Lauer <simon.lauer@posteo.de>
2024-05-16 16:05:57 +0200

    chore: update flake
    now supports both, mac and linux

--- default.nix
+++ default.nix
@@ -2,17 +2,20 @@
 with pkgs.python311Packages;
   buildPythonApplication rec {
     pname = "git_alert";
-    version = "0.3.1";
+    version = "0.3.3";
     format = "pyproject";
     src = fetchPypi {
       inherit pname version;
-      sha256 = "sha256-q0nZY/1LcUI1uJGTECA3/Nwdr4zJJVh6K/QNtuJcybc=";
+      sha256 = "sha256-Dk4h4UZRes0QKHN5dd79wFEqXHC8Qn6LPtLXfKhr0FA=";
     };
+    propagatedBuildInputs = [
+      rich
+    ];
     build-system = [
       poetry-core
     ];
     meta = {
-      changelog = "https://github.com/nomisreual/git_alert/releases/tag/v0.3.1";
+      changelog = "https://github.com/nomisreual/git_alert/compare/v0.3.2...v0.3.3";
       description = "Checks a given path and its sub-directories for dirty git repositories.";
       homepage = "https://github.com/nomisreual/git_alert";
       license = lib.licenses.mit;
