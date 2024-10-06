25f7953b96f12f57d9cabc21bc96bb6598f4b7ae
Simon Antonius Lauer <simon.lauer@posteo.de>
2024-10-06 07:54:20 +0200

    feat: install dev version of local package

--- shell.nix
+++ shell.nix
@@ -1,8 +1,20 @@
 { pkgs ? import <nixpkgs> { } }:
+with pkgs.python311Packages;
+let
+  local = buildPythonApplication {
+    name = "git-alert-dev";
+    src = ./.;
+    propagatedBuildInputs = [ rich ];
+    build-system = [
+      poetry-core
+    ];
+  };
+in
 pkgs.mkShell {
   packages = [
     (pkgs.python311.withPackages (python-pkgs: with python-pkgs; [
       rich
+      local
     ]))
 
     (pkgs.poetry.override { python3 = pkgs.python311; })
