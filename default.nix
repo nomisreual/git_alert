7127549a191041f2250703c0768c547d27fd5f09
Simon Antonius Lauer <simon.lauer@posteo.de>
2025-04-18 12:55:05 +0200

    chore: rename build inputs
    Usage of `dependencies` rather than `propagatedBuildInputs` to utilize
    options specific to the python builder function.

--- default.nix
+++ default.nix
@@ -10,8 +10,11 @@ in
       inherit pname version;
       sha256 = "sha256-edGskDzNnzLOV2P2RKn88DtrwgYhx3f5WIHWfyxVJ80=";
     };
-    propagatedBuildInputs = [
+    dependencies = [
       rich
+    ];
+    nativeCheckInputs = [
+      pytest
     ];
     build-system = [
       hatchling
