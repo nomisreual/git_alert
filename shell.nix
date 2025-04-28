a1eba4f2bc4307c75ee8e1efabb004222fa383a7
Simon Antonius Lauer <simon.lauer@posteo.de>
2025-04-28 13:31:52 +0200

    chore: update shell

--- shell.nix
+++ shell.nix
@@ -1,24 +1,24 @@
-{ pkgs ? import <nixpkgs> { } }:
-with pkgs.python311Packages;
-let
+{pkgs ? import <nixpkgs> {}}:
+with pkgs.python311Packages; let
   manifest = (pkgs.lib.importTOML ./pyproject.toml).project;
   local = buildPythonPackage {
     name = manifest.name;
     src = ./.;
-    propagatedBuildInputs = [ rich ];
+    propagatedBuildInputs = [rich pygit2];
     build-system = [
       hatchling
     ];
     pyproject = true;
   };
 in
-pkgs.mkShell {
-  packages = [
-    (pkgs.python311.withPackages (python-pkgs: with python-pkgs; [
-      rich
-      local
-    ]))
-
-    (pkgs.poetry.override { python3 = pkgs.python311; })
-  ];
-}
+  pkgs.mkShell {
+    packages = [
+      (pkgs.python311.withPackages (python-pkgs:
+        with python-pkgs; [
+          rich
+          pygit2
+          local
+        ]))
+      pkgs.pre-commit
+    ];
+  }
