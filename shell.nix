1dbb56f18f674d879163e9af8385bd64339323c0
Simon Antonius Lauer <nomisreual@proton.me>
2025-10-18 13:36:01 +0200

    fix: nix shell

--- shell.nix
+++ shell.nix
@@ -1,24 +1,12 @@
 {pkgs ? import <nixpkgs> {}}:
-with pkgs.python311Packages; let
-  manifest = (pkgs.lib.importTOML ./pyproject.toml).project;
-  local = buildPythonPackage {
-    name = manifest.name;
-    src = ./.;
-    propagatedBuildInputs = [rich pygit2];
-    build-system = [
-      hatchling
-    ];
-    pyproject = true;
-  };
-in
-  pkgs.mkShell {
-    packages = [
-      (pkgs.python311.withPackages (python-pkgs:
-        with python-pkgs; [
-          rich
-          pygit2
-          local
-        ]))
-      pkgs.pre-commit
-    ];
-  }
+pkgs.mkShell {
+  packages = [
+    (pkgs.python311.withPackages (python-pkgs:
+      with python-pkgs; [
+        rich
+        pygit2
+        flake8
+      ]))
+    pkgs.pre-commit
+  ];
+}
